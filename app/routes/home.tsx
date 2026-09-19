b6a3fa25de2a87bf9783aa1a5b648d6114e826a9
Vitor Milano <milano1605@gmail.com>
2026-09-19 18:06:16 -0300

    feat: add search page placeholder and route

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,16 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
+import Search from '~/search';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: 'GitHub Username Search' },
+    {
+      name: 'description',
+      content: 'This is the route responsible for the username search',
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Search />;
 }
---
+++ app/search/index.tsx
@@ -0,0 +1,63 @@
+import { useCallback, useState } from 'react';
+import { Button, Col, Container, Form, InputGroup, Row } from 'react-bootstrap';
+import { useGetUsers } from '~/hooks/useGetUsers';
+
+export default function Search() {
+  const [username, setUsername] = useState<string>('');
+  const [searchValue, setSearchValue] = useState<string>('');
+  const { data } = useGetUsers(username);
+  const handleSubmit = useCallback(
+    (e: React.SubmitEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setUsername(searchValue);
+    },
+    [searchValue]
+  );
+  return (
+    <Container className="d-flex flex-column justify-content-center mt-4">
+      <Row className="justify-content-center align-items-center ">
+        <Col>
+          <h1 className="text-center">Busca Github</h1>
+          <p className="text-center">
+            Consulte qualquer usuário do GitHub e explore os repositorios deles
+          </p>
+        </Col>
+      </Row>
+      <Row className="justify-content-center align-items-center">
+        <Col>
+          <Form onSubmit={handleSubmit}>
+            <InputGroup className="mb-3">
+              <Form.Control
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+                placeholder="Usuário do GitHub"
+                aria-label="Buscar usuário do GitHub"
+              />
+              <Button
+                type="submit"
+                variant="outline-secondary"
+                id="search-button"
+              >
+                Buscar
+              </Button>
+            </InputGroup>
+          </Form>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="mb-2" xs={12} md={6} lg={4}>
+          <span className="fw-bold">Nome: </span>
+          <span> {data?.data.name}</span>
+        </Col>
+        <Col className="mb-2" xs={12} md={6} lg={4}>
+          <span className="fw-bold">Bio: </span>
+          <span> {data?.data.bio}</span>
+        </Col>
+        <Col className="mb-2" xs={12} md={6} lg={4}>
+          <span className="fw-bold">Total de repositórios: </span>
+          <span> {data?.data.public_repos}</span>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
