0ab6cf6d4e4b61b597205a9f0d371b74feea69ed
Vitor Milano <milano1605@gmail.com>
2026-09-21 11:12:52 -0300

    feat: enhance RepoDetail placeholder

--- app/pages/RepoDetail.tsx
+++ app/pages/RepoDetail.tsx
@@ -1,5 +1,6 @@
-import { Col, Container, Row } from 'react-bootstrap';
-import { useParams } from 'react-router';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { useNavigate, useParams } from 'react-router';
+import { InfoText } from '~/components/InfoText';
 import { useGetRepo } from '~/hooks/useGetRepo';
 
 export default function RepoDetail() {
@@ -7,12 +8,44 @@ export default function RepoDetail() {
     username: string;
     repository: string;
   }>();
-  const { data } = useGetRepo({ owner: username, repository });
-
+  const { data, isLoading } = useGetRepo({ owner: username, repository });
+  const navigate = useNavigate();
   return (
     <Container>
       <Row>
-        <Col>{data?.data.name}</Col>
+        <Col>
+          <Button variant="primary" onClick={() => navigate(-1)}>
+            Voltar
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <InfoText
+            isLoading={isLoading}
+            text={data?.data.name ?? 'Repositório não encontrado'}
+            title="Nome"
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <InfoText
+            text={data?.data.description ?? 'Nenhuma descrição disponível'}
+            isLoading={isLoading}
+            title="Descrição"
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <InfoText
+            text={data?.data.stargazers_count?.toString()}
+            isLoading={isLoading}
+            title="Estrelas"
+            length={3}
+          />
+        </Col>
       </Row>
     </Container>
   );
