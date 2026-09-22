c010e4f60084daca49d1f7377a9e2d4fc5e0397d
Vitor Milano <milano1605@gmail.com>
2026-09-22 10:42:12 -0300

    feat: add title

--- app/root.tsx
+++ app/root.tsx
@@ -35,6 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <title>Desbravador Challenge</title>
       </head>
       <body className="bg-dark text-light">
         {children}
