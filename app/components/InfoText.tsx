b0a6c7c64c620faab343e23fde38afad8baa8fba
Vitor Milano <milano1605@gmail.com>
2026-09-22 07:08:16 -0300

    feat: change textskeleton into a simpler component

--- app/components/InfoText.tsx
+++ app/components/InfoText.tsx
@@ -4,23 +4,15 @@ export function InfoText({
   text,
   isLoading,
   title,
-  length = 20,
 }: {
   text?: string;
   title?: string;
   isLoading: boolean;
-  length?: number;
 }) {
   return (
     <div className="d-flex align-items-baseline gap-2 mb-1">
       <span className="fw-bold">{title}:</span>
-      <TextSkeleton
-        text={text}
-        isLoading={isLoading}
-        length={length}
-        speed={40}
-        revealSpeed={50}
-      />
+      <TextSkeleton text={text} isLoading={isLoading} />
     </div>
   );
 }
